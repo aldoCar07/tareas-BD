7aeab27d4599740f66e0c18e5e0559908b4bd760
aldoCar07 <aldo.ecj@gmail.com>
2022-04-30 15:28:43 -0500

    Tareas 1 y 2

--- Tarea 1.sql
+++ Tarea 1.sql
@@ -111,3 +111,40 @@ select o.ship_country, sum(od.quantity)
 from order_details od join orders o on (od.order_id = o.order_id)
 group by o.ship_country 
 order by sum(od.quantity) desc limit 1;
+
+
+
+------------Tarea 2-------------------------------
+
+create table heroes(
+hero_id numeric(4,0) constraint pk_heroes primary key,
+nombre varchar(50) not null,
+email varchar(80) not null
+);
+
+create sequence hero_id_hero_seq start 1 increment 1;
+alter table heroes 
+alter column hero_id set default nextval('hero_id_hero_seq');
+
+insert into heroes(nombre, email) 
+values('Wanda Maximoff', 'wanda.maximoff@avengers.org'),
+('Pietro Maximoff', 'pietro@mail.sokovia.ru'),
+('Erik Lensherr', 'fuck_you_charles@brotherhood.of.evil.mutants.space'),
+('Charles Xavier', 'i.am.secretely.filled.with.hubris@xavier-school-4-gifted-youngste.'),
+('Anthony Edward Stark', 'iamironman@avengers.gov'),
+('Steve Rogers','americas_ass@anti_avengers'),
+('The Vision','vis@westview.sword.gov'),
+('Clint Barton', 'bul@lse.ye'),
+('Natasja Romanov', 'blackwidow@kgb.ru'),
+('Thor', 'god_of_thunder-^_^@royalty.asgard.gov'),
+('Logan','wolverine@cyclops_is_a_jerk.com'),
+('Ororo Monroe','ororo@weather.co'),
+('Scott Summers','o@x'),
+('Nathan Summers','cable@xfact.or'),
+('Groot','iamgroot@asgardiansofthegalaxyledbythor.quillsux'),
+('Nebula','idonthaveelektras@complex.thanos'),
+('Gamora','thefiercestwomaninthegalaxy@thanos.'),
+('Rocket','shhhhhhhh@darknet.ru');
+
+SELECT * FROM heroes WHERE email NOT LIKE '%@%.%';
+
